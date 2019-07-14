2cd94f184a050a51c372c9b1eae0be2aade76508
amit goley <amit.imsmca@gmail.com>
2019-07-14 15:08:48 -0400

    added zuul apigateway within eurekaserver

--- EurekaServer/src/main/java/com/osp/eurekabootserver/EurekaBootServer.java
+++ EurekaServer/src/main/java/com/osp/eurekabootserver/EurekaBootServer.java
@@ -4,7 +4,9 @@ package com.osp.eurekabootserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaBootServer {
